f28e6b26b3dacd3782fe99b09254a30799bc1eb3
danpegollo <danielpegollo29@gmail.com>
2026-09-17 13:52:19 -0300

    adiciona funcoes assistente_faixa_dinamica, analise_risco_frontal, calcular_distancia_segura, extrair_mediana

--- main.c
+++ main.c
@@ -9,6 +9,10 @@ int carregar_dados_iniciais(float velocidade[][2], float sensores_frotais[][3], 
 int inserir_amostras(float velocidade[][2], float sensores_frotais[][3], float sensores_laterais[][2], int total_amostras);
 void processar_relatorio(float velocidade[][2], float sensores_frontais[][3], float sensores_laterais[][2], 
     float processamento[][2], int status[][3], int total_amostras, float atrito, int sensibilidade);
+void calcular_distancia_segura(float processamento[][2], float velocidade[][2], int total_amostras, float atrito, int sensibilidade);
+void extrair_mediana(float processamento[MAX_AMOSTRAS][2], float sensores_frontais[MAX_AMOSTRAS][3], int total_amostras);
+void analise_risco_frontal(float velocidades[][2], float processamento[][2], int status[][3], int n);
+void assistente_faixa_dinamica(float velocidades[][2], float sensores_laterais[][2], int status[][3], int n);
 
 int main(){
     //matrizes exigidas
@@ -41,10 +45,10 @@ int main(){
     //opções do menu
         switch (opcao) {
         case 1:
-            carregar_dados_iniciais(velocidade, sensores_frontais, sensores_laterais, total_amostras);
+            total_amostras = carregar_dados_iniciais(velocidade, sensores_frontais, sensores_laterais, total_amostras);
             break;
         case 2:
-            inserir_amostras(velocidade, sensores_frontais, sensores_laterais, total_amostras);
+            total_amostras = inserir_amostras(velocidade, sensores_frontais, sensores_laterais, total_amostras);
             break;
         case 3:
             processar_relatorio(velocidade, sensores_frontais, sensores_laterais, processamento, status, total_amostras, atrito, sensibilidade);
@@ -75,6 +79,7 @@ int carregar_dados_iniciais(float velocidade[][2], float sensores_frontais[][3],
     return 50; //total de amostras preenchidas
 }
 int inserir_amostras(float velocidade[][2], float sensores_frotais[][3], float sensores_laterais[][2], int total_amostras){
+    //verifica se a qtd amostras é maior que o limite
     if(total_amostras >= MAX_AMOSTRAS){
         printf("Erro, atingiu o limite máximo de amostras %d\n", MAX_AMOSTRAS);
         return total_amostras;
@@ -105,4 +110,105 @@ int inserir_amostras(float velocidade[][2], float sensores_frotais[][3], float s
     printf("Amostra inserida\n");
     // retorna nova amostra
     return total_amostras + 1;
+}
+void extrair_mediana(float processamento[MAX_AMOSTRAS][2], float sensores_frontais[MAX_AMOSTRAS][3], int total_amostras) {
+    for (int i=0; i<total_amostras; i++) {
+        float a = sensores_frontais[i][0];
+        float b = sensores_frontais[i][1];
+        float c = sensores_frontais[i][2];
+        if (a > b) {
+            /*se "a" maior que "b", "a" recebe "b", e "b" recebe valor de "a"*/
+            float temp = a;
+            a = b;
+            b = temp;
+        }
+        if (b > c) {
+            /*se "b" maior que "c", "b" recebe "c", e "c" recebe valor de "b"
+            depois dessa comparação, c se torna o maior valor*/
+            float temp1 = b;
+            b = c;
+            c = temp1;
+        }
+        if (a > b) {
+            /*no fim, a e b podem não estar ordenados
+        por conta da comparação b>c, então, faz a comparação denovo*/
+            float temp = a;
+            a = b;
+            b = temp;
+        }
+    /*Retorna a mediana (sempre sendo b)*/    
+    processamento[i][0] = b;
+    }
+}
+void calcular_distancia_segura(float processamento[MAX_AMOSTRAS][2], float velocidade[MAX_AMOSTRAS][2], int total_amostras, float atrito, int sensibilidade) {
+    float tempo_reacao;
+    /*traduz a sensibilidade escolhida para o tempo de reação correspondente*/
+    if (sensibilidade == 1) {
+        tempo_reacao = 1.0;
+    } else if (sensibilidade == 2) {
+        tempo_reacao = 1.5;
+    } else {
+        tempo_reacao = 2.0;
+    }
+
+    for (int i = 0; i < total_amostras; i++) {
+        /*converte a velocidade atual de km/h para m/s antes de usar na fórmula*/
+        float v_ms = velocidade[i][0] / 3.6;
+
+        /*aplica a fórmula: (v x tempoReacao) + (v² / (2 x atrito x 9.81))*/
+        float distancia_segura = (v_ms * tempo_reacao) + ((v_ms * v_ms) / (2 * atrito * 9.81));
+
+        /*grava na coluna 1 da matriz de processamento (distância segura calculada)*/
+        processamento[i][1] = distancia_segura;
+    }
+}
+void analise_risco_frontal(float velocidades[][2], float processamento[][2], int status[][3], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+            float velocidade_relativa = velocidades[i][0] - velocidades[i][1];
+            
+            if (velocidade_relativa <= 0) {
+                status[i][0] = 0;
+            }
+            else {
+                float distancia_segura = processamento[i][1];
+                float distancia_validada = processamento[i][0];
+
+                if (distancia_validada >= distancia_segura) {
+                    status[i][0] = 0;
+                }
+                else if (distancia_validada >= (distancia_segura / 2.0)) {
+                    status[i][0] = 1;
+                }
+                else {
+                    status [i][0] = 2;
+                }
+            }
+    }
+}
+void assistente_faixa_dinamica(float velocidades[][2], float sensores_laterais[][2], int status[][3], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        float margem_dinamica = 0.50;
+        float velocidade_atual = velocidades[i][0];
+        
+        if (velocidade_atual > 80.0) {
+            margem_dinamica += 0.01 * (velocidade_atual - 80.0);
+        }
+
+        int k;
+        for (k = 0; k < 2; k++) {
+            float leitura = sensores_laterais[i][k];
+
+            if (leitura < margem_dinamica) {
+                status[i][k + 1] = 2;
+            }
+            else if (leitura < (margem_dinamica + 0.20)) {
+                status[i][k + 1] = 1;
+            }
+            else {
+                status[i][k + 1] = 0;
+            }
+        }
+    }
 }
